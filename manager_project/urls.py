739c4e946a79b010eb171322a6b4c055360758cb
Yu Ishiguro <yuuis.egr@gmail.com>
2018-11-20 21:51:20 +0900

    add some templates

--- manager_project/urls.py
+++ manager_project/urls.py
@@ -14,7 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.conf.urls import url
+
+import manager.views as manager_view
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
