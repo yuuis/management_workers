ea320c4d30b8f331597046152f71ea01c8e34433
Yu Ishiguro <yuuis.egr@gmail.com>
2018-11-22 15:40:41 +0900

    change setting

--- manager_project/settings.py
+++ manager_project/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = ox.path.join(BASE_DIR, 'assets')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static')
-)
+# STATIC_ROOT = ox.path.join(BASE_DIR, 'assets')
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static')
+# )
